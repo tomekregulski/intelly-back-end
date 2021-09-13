251de80208cea5eecbcd1eb4145b0598c221ea3b
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-09-13 13:04:50 -0400

    Add root response for server

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -2,14 +2,15 @@ const router = require('express').Router();
 const { User } = require('../models');
 
 router.get('/', async (req, res) => {
-  try {
-    const allUsers = await User.findAll();
-    const userData = allUsers.map((user) => user.get({ plain: true }));
-    res.status(200).json(userData);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+  res.send('hello');
+  // try {
+  //   const allUsers = await User.findAll();
+  //   const userData = allUsers.map((user) => user.get({ plain: true }));
+  //   res.status(200).send(userData);
+  // } catch (err) {
+  //   console.log(err);
+  //   res.status(500).json(err);
+  // }
 });
 
 module.exports = router;
