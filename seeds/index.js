26fe49c1f3026e16ed68316cf4344f9ef26569bc
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-12-10 14:27:25 -0500

    data-modeling

--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,7 @@
-// const seedWholeFoodsData = require('./wholeFoodsSeeds');
-// const { WholeFoodsWeeklyArchive } = require('../models');
-// const wholeFoodsWeeklyArchiveSeeds = require('./wholeFoodsWeeklyArchiveSeeds.json');
-// const wholeFoodsTimeframeDataSeeds = require('./wholeFoodsTimeframeDataSeeds.json');
-// const simmerSasoSeeds = require('./simmerSasoSeeds.json');
 const { WholeFoodsTimeframeData } = require('../models');
 const wfmTimeframeSeed = require('./wfmTimeframeSeed.json');
 
 const sequelize = require('../config/connection');
-
-// const seedWholeFoodsWeeklyArchive = () =>
-//   WholeFoodsWeeklyArchive.bulkCreate(wholeFoodsWeeklyArchiveSeeds);
 
 const seedWholeFoodsTimeframeData = () =>
   WholeFoodsTimeframeData.bulkCreate(wfmTimeframeSeed);
@@ -17,12 +9,6 @@ const seedWholeFoodsTimeframeData = () =>
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-
-  // await seedWholeFoodsData();
-  // console.log('\n----- WHOLE FOODS DATA SEEDED -----\n');
-
-  // await seedWholeFoodsWeeklyArchive();
-  // console.log('\n----- WHOLE FOODS WEEKLY ARCHIVE SEEDED -----\n');
 
   await seedWholeFoodsTimeframeData();
   console.log('\n----- WHOLE FOODS TIMEFRAME DATA SEEDED -----\n');
